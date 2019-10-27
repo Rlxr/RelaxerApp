7ac1fa8ba34298ea1ff47c6cd5011f62bb86c50c
Ulzii <uxb6317@rit.edu>
2019-10-27 12:36:10 -0400

    added simple descriptions for preview cards

--- src/data.js
+++ src/data.js
@@ -3,64 +3,57 @@ const DATA = [
     name: 'Waves',
     communityCreated: false,
     image: './images/waves.jpg',
-    details:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum.'
+    details: 'Swirly waves.'
   },
   {
     name: 'Frozen Brush',
     communityCreated: false,
     image: './images/frozenBrush.jpg',
-    details:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum.'
+    details: 'Blue/green frozen looking lines drawn with finger drag.'
   },
   {
     name: 'Particle Plotter',
     communityCreated: false,
     image: './images/particlePlotter.jpg',
-    details:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum.'
+    details: 'Orange/yellow fire looking patterns made when finger dragged.'
   },
   {
     name: 'Sketch 1',
     communityCreated: true,
     image: './images/sketch1.jpg',
-    details:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum.'
+    details: 'Rainbow diagnal lines. Tap to add, slide to blacken (erase).'
   },
   {
     name: 'Sketch 2',
     communityCreated: true,
     image: './images/sketch2.jpg',
-    details:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum.'
+    details: 'Gradient light random a animation.'
   },
   {
     name: 'Sketch 3',
     communityCreated: true,
     image: './images/sketch3.jpg',
-    details:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum.'
+    details: '3 light gradients, tap to blur/lighten.'
   },
   {
     name: 'Sketch 4',
     communityCreated: true,
     image: './images/sketch4.jpg',
-    details:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum.'
+    details: 'Tap to insert a color swapper circle, hold and drag to erase.'
   },
   {
     name: 'Sketch 5',
     communityCreated: true,
     image: './images/sketch5.jpg',
     details:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum.'
+      'Animation of randomly colored bubbles (tap and hold to zoom in, tap again to go back).'
   },
   {
     name: 'Sketch 6',
     communityCreated: true,
     image: './images/sketch6.jpg',
     details:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum.'
+      'Tap to add black vertical lines, drag finger for black diagnal lines.'
   }
 ];
 
